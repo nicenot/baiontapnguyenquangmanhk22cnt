09a441b745d1f5f6af60759ab551cf9ce50ef84e
NGUYỄN QUNAG MẠNH <146909381+nicenot@users.noreply.github.com>
2024-07-15 22:07:57 +0700

    1

---
+++ nqm-2210900039-de01-app/src/NqmApp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function nqmApp() {
+  return (
+    <div className='container border my'>
+      <h1>bài đánh giá hết học phần - Nguyễn Quang Mạnh : 2210900039 </h1>
+      <hr/>
+      
+    </div>
+  )
+}
